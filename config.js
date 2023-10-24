3da76f35cf1887786f7035eef4faf61c46d38649
f9charlie <125704327+f9charlie@users.noreply.github.com>
2023-10-24 18:20:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['918792786331', 'charlie Official²⁰²³', true],
+  ['918792786331', 'charlie Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['94740804536','94740804536'] 
-global.prems = ['94740804536', '94740804536', '94740804536']
+global.mods = ['918792786331','918792786331'] 
+global.prems = ['918792786331', '918792786331', '918792786331']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,19 +47,19 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = '💝 charlie Hentai 💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/Dinuwa Official²⁰²³\n' 
-global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
+global.packname = '💝 charlie Hentai 💝 Created By 💝 f9 CHARLIE Official²⁰²³ 💝' 
+global.author = '💝 F9 charlie  Official²⁰²³ 💝' 
+global.igfg = '▢ Follow on Instagram\nhttps://instagram.com/f9____x_____charli____?igshid=MzNlNGNkZWQ4Mg==\n' 
+global.dygp = 'https://chat.whatsapp.com/KaUv8hJ99bpHgyu99PRXH6'
 global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
-global.fgyt = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
+global.fgyt = 'https://chat.whatsapp.com/KaUv8hJ99bpHgyu99PRXH6'
+global.fgpyp = 'https://chat.whatsapp.com/KaUv8hJ99bpHgyu99PRXH6'
+global.fglog = 'https://i.imgur.com/wNc6FPg.jpg' 
 
 
-global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
+global.wait = '*💝 _Waiting Bro Uploading by 💝 charlie  Hentai 💝..._*'
 global.rwait = '🇱🇰'
 global.dmoji = '🤣'
 global.done = '👍'
